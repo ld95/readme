20470348a5b1e13c047cd0fea9755549eb6d2e85
Yansong Liu <yl2223@njit.edu>
2019-02-23 19:30:17 -0500

    Fix:Revised index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -8,69 +8,127 @@
 </head>
 <body>
 <h1>More power.More performance.More pro</h1>
-</body>
-</html>
 
 <?php
 /**
  * Created by PhpStorm.
- * User: yansong
- * Date: 2/18/19
- * Time: 11:49 PM
+ * User: peiran
+ * Date: 10/6/18
+ * Time: 9:56 PM
  */
 
 
 
 main::start("example.csv");
+
 class main  {
     static public function start($filename) {
         $records = csv::getRecords($filename);
         $table = html::generateTable($records);
+        echo $table;
+        //echo 'test';
     }
 }
-class html {
-    public static function generateTable($records) {
+class html
+{
+    public static function generateTable($records)
+    {
+        // start table
+        $html = '<table class="table">';
 
-        $html = '<table>';
         $count = 0;
         foreach ($records as $record) {
-            if($count == 0) {
-                $array = $record->returnArray();
-                //$fields = array_keys($array);
-                //$values = array_values($array);
-                $html .= '<tr>';
-                foreach($array as $key =>$value)
-                {
-                    $html .='<th>'.htmlspecialchars($key) . '</th>';
-                }
-                $html .='</tr>';
+            $array = html::makearray($record);
+            if ($count == 0) {
+                $html = html::tablehead($array,$html);
+                $html = html::tablerows($array,$html);
 
-                $html .='<tr>';
-                foreach($array as $key =>$value)
-                {
-                    $html .='<th>'.htmlspecialchars($value) . '</th>';
-                }
-                $html .='</tr>';
-
-                //print_r($fields);
-                //print_r($values);
             } else {
-                $array = $record->returnArray();
-                //$values = array_values($array);
-                $html .='<tr>';
-                foreach($array as $key =>$value)
-                {
-                    $html .='<th>'.htmlspecialchars($value) . '</th>';
-                }
-                $html .='</tr>';
-                //print_r($values);
+                $html = html::tablerows($array,$html);
             }
             $count++;
         }
-        $html .='</table>';
-        echo $html;
+        $html = html::endtable($html);
+        return $html;
+    }
+
+    static public function makearray($record)
+    {
+        $ma = $record->returnArray();
+        return $ma;
+    }
+
+    static public function sethead($sh)
+    {
+        $sh .= '<thead class="thead-light">';
+        return $sh;
+    }
+    static public function makerow($mk)
+    {
+        $mk .= '<tr>';
+        return $mk;
+    }
+    static public function endrow($er)
+    {
+        $er .= '</tr>';
+        return $er;
+    }
+    static public function headline($hd, $key)
+    {
+        $hd .= '<th>' . htmlspecialchars($key) . '</th>';
+        return $hd;
+    }
+    static public function tableline($td, $value)
+    {
+        $td .= '<td>' . htmlspecialchars($value) . '</td>';
+        return $td;
+    }
+    static public function tablehead($ar,$hl)
+    {
+        $html = html::sethead($hl);
+        $html = html::makerow($html);
+        foreach ($ar as $key => $value) {
+            $html = html::headline($html, $key);
+            //$html .= '<th>' . htmlspecialchars($key) . '</th>';
+        }
+        $html = html::endrow($html);
+        $html = html::endhead($html);
+        return $html;
+    }
+    static public function tablerows($ar,$hl)
+    {
+        $hl = html::tablebody($hl);
+        $hl = html::makerow($hl);
+        foreach ($ar as $key => $value) {
+            $hl = html::tableline($hl, $value);
+            //$html .= '<td>' . htmlspecialchars($value) . '</td>';
+        }
+        $hl = html::endrow($hl);
+        $hl = html::endbody($hl);;
+        return $hl;
+    }
+    static public function tablebody($tb)
+    {
+        $tb .= '<tbody>';
+        return $tb;
+    }
+    static public function endbody($eb)
+    {
+        $eb .= '</tbody>';
+        return $eb;
+    }
+    static public function endhead($eh)
+    {
+        $eh .= '</thead>';
+        return $eh;
+    }
+    static public function endtable($et)
+    {
+        $et .= '</table>';
+        return $et;
     }
 }
+
 class csv {
     static public function getRecords($filename) {
         $file = fopen($filename,"r");
@@ -112,3 +170,8 @@ class recordFactory {
         return $record;
     }
 }
+
+?>
+
+</body>
+</html>
